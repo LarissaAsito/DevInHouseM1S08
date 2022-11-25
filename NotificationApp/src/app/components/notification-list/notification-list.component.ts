48409b58aad73ffcf131ea45585bd3cb975c1901
Larissa <larissa.asito@gmail.com>
2022-11-25 20:58:07 -0300

    Adicionando exercicio 7

--- NotificationApp/src/app/components/notification-list/notification-list.component.ts
+++ NotificationApp/src/app/components/notification-list/notification-list.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./notification-list.component.scss']
 })
 export class NotificationListComponent {
-
+  notificacoes = [
+    {
+      titulo: 'WhatsApp',
+      subtitulo: 'Novas mensagens.',
+      data: '25/11/2022 20h00'
+    },
+    {
+      titulo: 'Intagram',
+      subtitulo: 'Novo reel.',
+      data: '25/11/2022 19h00'
+    },
+    {
+      titulo: 'YouTube',
+      subtitulo: 'Novo vídeo.',
+      data: '25/11/2022 20h30'
+    }
+  ]
 }
